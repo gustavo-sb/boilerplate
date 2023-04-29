78a875371d70542bbce921b6b38d7d8cfc07b799
gustavo-sb <gustavosb862@gmail.com>
2023-04-29 11:12:41 -0300

    :tada: first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// `next-pwa` config should be passed here
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  disable: process.env.NODE_ENV === 'development'
+})
 
-module.exports = nextConfig
+// Use `withPWA` and pass general Next.js config
+module.exports = withPWA({
+  reactStrictMode: true,
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false }
+    return config
+  },
+  images: {
+    domains: [
+      'pbs.twimg.com',
+      'img.icons8.com',
+      'gateway.moralisipfs.com',
+      'ipfs.moralis.io',
+      'lh3.googleusercontent.com',
+      'www.artnews.com'
+    ]
+  },
+  output: 'standalone'
+})
